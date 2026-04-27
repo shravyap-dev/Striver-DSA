f7fe65e4a4eedd61b7e726f18dd5d7a21b7d8484
shravyap-dev <shravyaprakash.16@gmail.com>
2026-04-27 21:23:35 +0530

    Update README for Day 2

---
+++ Basics/valid_palindrome.java
@@ -0,0 +1,26 @@
+// Problem: Valid Palindrome (LeetCode 125)
+// Pattern: String processing
+// TC: O(n), SC: O(1)
+
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+        
+    }
+}
